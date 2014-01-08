4b0fcdd1b728a40490ea5597678c9ad46d9d05c0
Stian Eikeland <stian@eikeland.se>
2014-01-08 21:13:02 +0100

    Set up project

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject mengenlehreuhr "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-time "0.6.0"]
+                 [quil "1.6.0"]]
+  :plugins [[quickie "0.2.4"]]
+  :main mengenlehreuhr.ui
+  )
